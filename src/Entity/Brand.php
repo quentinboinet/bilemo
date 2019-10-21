81edab0d40e8dd6dcd179b3ca27c2069a8bf36a7
quentinboinet <quentinboinet@live.fr>
2019-10-21 14:26:26 +0200

    Brand & Mobile
    Brand + mobile resources initiation
    Plus filtering on mobile

---
+++ src/Entity/Brand.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource(
+ *     collectionOperations={},
+ *     itemOperations={"get"}
+ * )
+ * @ORM\Entity(repositoryClass="App\Repository\BrandRepository")
+ */
+class Brand
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $name;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Mobile", mappedBy="brand")
+     */
+    private $mobiles;
+
+    public function __construct()
+    {
+        $this->mobiles = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Mobile[]
+     */
+    public function getMobiles(): Collection
+    {
+        return $this->mobiles;
+    }
+
+    public function addMobile(Mobile $mobile): self
+    {
+        if (!$this->mobiles->contains($mobile)) {
+            $this->mobiles[] = $mobile;
+            $mobile->setBrand($this);
+        }
+
+        return $this;
+    }
+
+    public function removeMobile(Mobile $mobile): self
+    {
+        if ($this->mobiles->contains($mobile)) {
+            $this->mobiles->removeElement($mobile);
+            // set the owning side to null (unless already changed)
+            if ($mobile->getBrand() === $this) {
+                $mobile->setBrand(null);
+            }
+        }
+
+        return $this;
+    }
+}
---
+++ src/Entity/Mobile.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiFilter;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\RangeFilter;
+
+/**
+ * @ApiResource(
+ *     collectionOperations={"get"},
+ *     itemOperations={"get"},
+ *     attributes={
+ *          "pagination_items_per_page"=10
+ *     }
+ * )
+ * @ApiFilter(SearchFilter::class, properties={"model": "partial", "brand.name" : "exact"})
+ * @ApiFilter(RangeFilter::class, properties={"year"})
+ * @ORM\Entity(repositoryClass="App\Repository\MobileRepository")
+ */
+class Mobile
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Brand", inversedBy="mobiles")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $brand;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $model;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $picture;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $year;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getBrand(): ?Brand
+    {
+        return $this->brand;
+    }
+
+    public function setBrand(?Brand $brand): self
+    {
+        $this->brand = $brand;
+
+        return $this;
+    }
+
+    public function getModel(): ?string
+    {
+        return $this->model;
+    }
+
+    public function setModel(string $model): self
+    {
+        $this->model = $model;
+
+        return $this;
+    }
+
+    public function getPicture(): ?string
+    {
+        return $this->picture;
+    }
+
+    public function setPicture(?string $picture): self
+    {
+        $this->picture = $picture;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getYear(): ?int
+    {
+        return $this->year;
+    }
+
+    public function setYear(int $year): self
+    {
+        $this->year = $year;
+
+        return $this;
+    }
+}
